44a9d676428d607e021bc844e23fff88b6160820
fideloper <fideloper@gmail.com>
2015-03-08 08:32:07 -0500

    updates to Envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -17,12 +17,14 @@ $release = 'release_' . date('YmdHis');
 @task('fetch_repo')
     [ -d {{ $release_dir }} ] || mkdir {{ $release_dir }};
     cd {{ $release_dir }};
-    git clone {{ $repo }} {{ $release }};
+    git clone -b master {{ $repo }} {{ $release }};
 @endtask
 
 @task('run_composer')
     cd {{ $release_dir }}/{{ $release }};
-    composer install --prefer-dist;
+    composer install --prefer-dist --no-scripts;
+    php artisan clear-compiled --env=production;
+    php artisan optimize --env=production;
 @endtask
 
 @task('update_permissions')
@@ -32,6 +34,17 @@ $release = 'release_' . date('YmdHis');
 @endtask
 
 @task('update_symlinks')
+    cd {{ $release_dir }}/{{ $release }};
+    ln -nfs ../../.env .env;
+    chgrp -h www-data .env;
+
     ln -nfs {{ $release_dir }}/{{ $release }} {{ $app_dir }};
     chgrp -h www-data {{ $app_dir }};
+
+    rm -r {{ $release_dir }}/{{ $release }}/storage/logs;
+    cd {{ $release_dir }}/{{ $release }}/storage;
+    ln -nfs ../../../logs logs;
+    chgrp -h www-data logs;
+
+    sudo service php5-fpm reload;
 @endtask
